3254eb928e7f7a22736b4b36aa0be0a0f9e14a8a
nciganovic <nciganovic99@gmail.com>
2020-04-28 18:51:35 +0200

    show all categories on index.php page

--- models/categories/getCategories.php
+++ models/categories/getCategories.php
@@ -1,3 +1,6 @@
 <?php 
 
-//$sql = "SELECT * FROM categories"
+$sql = "SELECT * FROM categories";
+$stmt = $pdo->query($sql);
+$stmt->execute();
+$allCategories = $stmt->fetchAll();
--- views/categories/sidebar.php
+++ views/categories/sidebar.php
@@ -1,1 +1,11 @@
-<p>This is sidebar</p>
+<?php 
+
+include_once "models/categories/getCategories.php";
+
+?>
+
+<ul>
+<?php foreach($allCategories as $category): ?>
+    <li><?= $category["name"] ?></li>
+<?php endforeach ?>
+</ul>
