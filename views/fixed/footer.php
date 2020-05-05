043ff8ae268188ef75327e2f07a5839d5c2bd2b4
nciganovic <nciganovic99@gmail.com>
2020-05-05 12:23:15 +0200

    footer always on bottom

--- views/fixed/footer.php
+++ views/fixed/footer.php
@@ -1,6 +1,6 @@
-        <footer class="bg-dark">
+        <footer id="footer" class="bg-dark">
             <div class="col-12  container">
-                <p class="p-3 text-center text-light">This is footer</p>
+                <p class="p-3 text-center text-light m-0">This is footer</p>
             </div>
         </footer>
     </body>
--- views/fixed/head.php
+++ views/fixed/head.php
@@ -13,6 +13,10 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+
 </head>
 <body>
+  <div id="page-container">
     
