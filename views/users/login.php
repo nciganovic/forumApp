309fc0a42bfe73566ab2de9ac25e2464e31c3a5f
nciganovic <nciganovic99@gmail.com>
2020-05-06 17:40:01 +0200

    login form added

--- views/users/login.php
+++ views/users/login.php
@@ -1,7 +1,25 @@
 <div class="container">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 mt-3">
             <h1 class="text-center">Login</h1>
         </div>
+        <div class="col-8 m-auto">
+            <form method="POST">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" id="email" class="form-control" aria-describedby="emailHelp" name="email" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" id="password" class="form-control" name="password" placeholder="Password">
+                </div>
+                <button type="button" id="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+            <div class="message mt-3">
+                
+            </div>
+        </div>
+
     </div>
 </div>
