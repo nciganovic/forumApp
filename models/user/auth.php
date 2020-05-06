6db15dc9147309a31881e4c6e71a99d2464047f2
nciganovic <nciganovic99@gmail.com>
2020-05-06 20:29:00 +0200

    auth file added

---
+++ models/user/auth.php
@@ -0,0 +1,73 @@
+
+<?php
+    session_start();
+
+    require_once "../../config/connection.php";
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        return $data;
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(isset($_POST["email"]) && isset($_POST["password"]) && !empty($_POST["email"]) && !empty($_POST["password"])){
+
+        $email = test_input($_POST["email"]);
+        $password = test_input($_POST["password"]);
+        $isEmailValid = preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email);
+        
+        if($isEmailValid){
+
+            if(strlen($password) > 4 && strlen($password) < 26){
+
+            $sql = "SELECT id, username, password, role FROM users WHERE email = :email AND isverified=1";
+            
+            if($stmt = $pdo->prepare($sql)){
+                $stmt->bindParam(":email", $email);
+                $stmt->execute();
+                if($stmt->rowCount() == 1){
+                $users = $stmt->fetchAll();
+                if(password_verify($password, $users[0]["password"])){
+                    
+                    session_start();
+
+                    // Store data in session variables
+                    $_SESSION["userid"] = $users[0]["id"];
+                    $_SESSION["role"] = $users[0]["role"];
+                    $_SESSION["username"] = $users[0]["username"];                            
+                        
+                    // Redirect depends on role
+                    if($users[0]["role"] == 1){
+                    header("location: admin/dashboard.php");
+                    }
+                    else{
+                    header("location: index.php");
+                    }
+                    
+                }
+                else{
+                    $_SESSION["message"] = "Password is invalid.";
+                }
+                }
+                else{
+                $_SESSION["message"] = "Email doesn't exist.";
+                }
+            }
+            }
+            else{
+            $_SESSION["message"] = "Password is maximum 25 characters.";
+            }
+
+        }
+        else{
+            $_SESSION["message"] = "Email is in wrong format.";
+        }
+        }
+        else{
+        $_SESSION["message"] = "Email or password are not inserted.";
+        }
+    }
+
+    ?>
+
