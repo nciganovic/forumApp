f405aec5d4e0ea6d4b3d83275f29821ea0239bff
nciganovic <nciganovic99@gmail.com>
2020-05-06 19:31:55 +0200

    login form added

---
+++ assets/js/login.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    console.log("checklogin.js");
+
+    var localEmail = localStorage.getItem("email");
+    var localPsw = localStorage.getItem("psw");
+
+    $("#exampleInputEmail").val(localEmail);
+    $("#exampleInputPassword").val(localPsw);
+
+    $(".btn").click(function(){
+        var email = $("#email").val();
+        var psw = $("#password").val();
+
+        var emailRgx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var isValidEmail = emailRgx.test(email);
+
+        var isValidPsw = true;
+        if(psw.length > 25 || psw.length < 5){
+            isValidPsw = false;
+        }
+
+        $(".message").html("");
+    
+        if(!isValidEmail || !isValidPsw){
+
+            if(!isValidEmail){
+                $(".message").append("<p class='text-danger text-center'>Email is in wrong format.</p>");
+            }
+            if(!isValidPsw){
+                $(".message").append("<p class='text-danger text-center'>Wrong password.</p>");
+            }
+            
+        }
+        else{
+            //AJAX request
+            $.ajax({
+                url:"models/user/insertuser.php",
+                method:"post",
+                data:{
+                    email:email,
+                    password:psw,
+                },
+                dataType:"json",
+                success:function(data){
+                    console.log(data);
+                    
+                    if(data.result == "0"){
+                        $(".message").html(data.msg);
+                    }
+                    else{
+                        $(".message").html(data.msg);
+
+                        $("#email").val("");
+                        $("#password").val("");
+                    }
+                    
+                },
+                error:function(err){
+                    console.log(err);
+                }
+            })
+        }
+    })
+})
--- views/users/login.php
+++ views/users/login.php
@@ -23,3 +23,6 @@
 
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="assets/js/login.js"></script>
