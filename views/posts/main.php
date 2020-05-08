12b874973a4c935af217a88bed441f0c03dee2e1
nciganovic <nciganovic99@gmail.com>
2020-05-08 16:31:59 +0200

    show posts on main page

--- views/posts/main.php
+++ views/posts/main.php
@@ -1,4 +1,18 @@
- 
+<?php
+    require_once "models/posts/getpost.php";
+?>
+
 <?php if(isset($_SESSION["userid"])): ?>    
 <div class="mt-3"><a class="p-3 btn btn-success" href="index.php?page=createpost&width=1">Create post</a></div>
 <?php endif ?>
+
+<?php foreach($allPosts as $post): ?>
+    <ul>
+        <li><?= $post["id"] ?></li>
+        <li><?= $post["title"] ?></li>
+        <li><?= $post["name"] ?></li>
+        <li><?= $post["createdat"] ?></li>
+        <li><?= $post["username"] ?></li>
+    </ul>
+<?php endforeach ?>
+
