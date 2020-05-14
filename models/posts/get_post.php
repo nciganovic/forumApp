af4629a87ef80b2ab3275bc103e35b21e66bbfe9
nciganovic <nciganovic99@gmail.com>
2020-05-14 13:15:58 +0200

    changed name of file

---
+++ models/posts/get_post.php
@@ -0,0 +1,15 @@
+<?php 
+if(isset($_GET["id"])){
+    
+    $sql = "SELECT p.id, p.title, p.description, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
+            FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
+            INNER JOIN categories c ON p.categoryid = c.id
+            INNER JOIN users u ON p.userid = u.id
+            WHERE p.id = :id
+            GROUP BY p.id, p.title, p.createdat, c.name, u.username, u.id";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":id", $_GET["id"]);
+    $stmt->execute();
+    $post = $stmt->fetchAll();
+}
