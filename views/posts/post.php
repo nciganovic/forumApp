52d0e468f512b3c279dca87eb054160e306ddb8f
nciganovic <nciganovic99@gmail.com>
2020-05-15 12:01:36 +0200

    comment structure in html

--- views/posts/post.php
+++ views/posts/post.php
@@ -9,3 +9,65 @@ require_once "models/posts/get_post.php";
 <p><?= $post[0]["name"] ?></p>
 <p><?= $post[0]["username"] ?></p>
 <p><?= $post[0]["likes"] ?></p>
+
+<form class="mt-5">
+    <label for="exampleFormControlSelect2">Insert comment:</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <button class="btn btn-success mt-3" type="button">Send</button>
+</form>
+
+<div id="comments" class="mt-5 mb-5">
+    <div class="com-and-rep mt-4">
+        
+        <div class="comment">
+            <p class="mb-0">Level 1 comment</p>
+            <a href="#">reply</a>
+        </div>
+
+        <div class="replies pl-5 mt-4 border-left">
+
+            <div class="com-and-rep">
+
+                <div class="comment">
+                    <p class="mb-0">Level 2 comment</p>
+                    <a href="#">reply</a>
+                </div>
+
+                <div class="replies pl-5 mt-4 border-left">
+
+                    <div class="com-and-rep mt-4">
+                        
+                        <div class="comment">
+                            <p class="mb-0">Level 3 comment</p>
+                            <a href="#">reply</a>
+                        </div>
+                        
+                        <div class="replies border-left">
+
+                        </div>
+                    </div>
+
+                    <div class="com-and-rep mt-4">
+                        <p class="mb-0">Level 3 comment</p>
+                        <a href="#">reply</a>
+
+                        <div class="replies border-left">
+
+                        </div>
+                    </div>
+
+                    
+
+                </div>
+
+            </div>
+
+        </div>
+        
+    </div>
+
+</div>
+
+<div class="mt-5 mb-5">
+    <p>---------------------------</p>
+</div>
