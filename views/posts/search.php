f60cb03e18738d5f6a646852236c09885b2af21c
nciganovic <nciganovic99@gmail.com>
2020-05-18 12:31:27 +0200

    search html layout

--- views/posts/search.php
+++ views/posts/search.php
@@ -1,3 +1,9 @@
 <div class="col-12">
     <h1 class="text-center">Search</h1>
+    <div class="form-group">
+        <input type="email" id="search-txt" class="form-control" placeholder="Search post">
+        <button id="search-btn" class="btn btn-success mt-3">Search</button>
+  </div>
 </div>
+
+<script src="assets/js/search.js"></script>
