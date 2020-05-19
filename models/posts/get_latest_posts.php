738224e96dba97ded9467a223a19e43a40c9c847
nciganovic <nciganovic99@gmail.com>
2020-05-19 14:07:40 +0200

    set limit value on latest posts

--- models/posts/get_latest_posts.php
+++ models/posts/get_latest_posts.php
@@ -4,8 +4,10 @@ $sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.id as 'userid',
         INNER JOIN categories c ON p.categoryid = c.id
         INNER JOIN users u ON p.userid = u.id
         GROUP BY p.id, p.title, p.createdat, c.name, u.username, u.id
-        ORDER BY p.createdat DESC";
+        ORDER BY p.createdat DESC
+        LIMIT :limit";
 
-$stmt = $pdo->query($sql);
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
 $stmt->execute();
 $allPosts = $stmt->fetchAll();
