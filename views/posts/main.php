e904bffacf228467dd6ea94a5706e9f6b057e341
nciganovic <nciganovic99@gmail.com>
2020-05-19 14:25:33 +0200

    remove upvoting

--- views/posts/main.php
+++ views/posts/main.php
@@ -7,8 +7,6 @@
     else{
         require_once "models/posts/get_latest_posts.php";
     }
-
-    require_once "models/upvotes/get_all_user_upvotes.php";
 ?>
 
 <?php if(isset($_SESSION["userid"])): ?>    
@@ -23,29 +21,6 @@
         <li><?= $post["createdat"] ?></li>
         <li><?= $post["username"] ?></li>
         <li>Likes: <span class="l-<?= $post["id"] ?>"> <?= $post["likes"] ?> </span></li>
-        
-        <?php if(isset($_SESSION["userid"])): ?>
-            <?php $is_upvoted = false ?>
-            <?php foreach($upvoted_posts as $up): ?>
-                <?php if($up["postid"] == $post["id"]): ?>
-                    <?php $is_upvoted = true ?>
-                    <?php break ?>
-                <?php endif ?>
-            <?php endforeach ?>
-
-            <?php if(!$is_upvoted): ?>  
-                <?php if($post["userid"] != $_SESSION["userid"]): ?>
-                    <li><a href="#" class="upvote" post="<?= $post["id"] ?>" user="<?= $_SESSION["userid"] ?>">Upvote</a></li>
-                <?php else: ?>
-                    <li><a href="#">Your post</a></li>
-                <?php endif ?>
-            <?php else: ?>
-                <li><a href="#" >Already upvoted</a></li>
-            <?php endif ?>
-
-        <?php else: ?>
-            <li><a href="#" class="set-alert">Upvote</a></li>
-        <?php endif ?>
     </ul>
 <?php endforeach ?>
 
@@ -53,5 +28,4 @@
 
 <p class="mt-5 mb-5"><a href="#" id="load-more" offset="2">Load more</a></p>
 
-<script src="assets/js/upvotePost.js"></script>
 <script src="assets/js/loadMore.js"></script>
