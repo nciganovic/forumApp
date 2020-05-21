f547b7b4f8b07b21b1bba36bf1a845614fcb05da
nciganovic <nciganovic99@gmail.com>
2020-05-21 18:28:29 +0200

    validate password

--- assets/js/changePassword.js
+++ assets/js/changePassword.js
@@ -4,6 +4,48 @@ $(document).ready(function(){
     $(".btn").click(function(){
         console.log("click");
 
+        var oldPassword = $("#old-psw").val();
+        var newPassword1 = $("#new-psw-1").val();
+        var newPassword2 = $("#new-psw-2").val();
+
+        $(".message").html("");
+
+        var isValidPsw = true;
+        if(newPassword1.length > 25 || newPassword1.length < 5 || newPassword2.length > 25 || newPassword2.length < 5){
+            $(".message").append("<p class='text-danger text-center'>New password is invalid. Must be between 5 and 25 characters.</p>");
+            isValidPsw = false;
+        }
+
+        if(newPassword1 != newPassword2){
+            $(".message").append("<p class='text-danger text-center'>New password are not identical.</p>");
+            isValidPsw = false;
+        }
+
+        if(oldPassword < 5 || oldPassword > 25){
+            $(".message").html("<p class='text-danger text-center'>Wrong current password.</p>");
+            isValidPsw = false;
+        }
+
+        if(isValidPsw){
+            //AJAX request
+            $.ajax({
+                url:"models/user/update_password.php",
+                method:"post",
+                data:{
+                    oldPassword: oldPassword,
+                    newPassword1:newPassword1,
+                    newPassword2:newPassword2
+                },
+                dataType:"json",
+                success:function(data){
+                    console.log(data);
+                },
+                error:function(err){
+                    console.log(err);
+                }
+            })
+        }
+
         
 
     })
