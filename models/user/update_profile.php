1dbf20dc2bed9c0edca0c3970a089dce678d1845
nciganovic <nciganovic99@gmail.com>
2020-05-21 15:13:17 +0200

    inset dat a and uplaod image

--- models/user/update_profile.php
+++ models/user/update_profile.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+require_once "../../config/connection.php";
+
 if(isset($_POST["username"]) && isset($_POST["bio"])){
     $username = $_POST["username"];
     $bio = trim($_POST["bio"]);
@@ -8,15 +11,22 @@ if(isset($_POST["username"]) && isset($_POST["bio"])){
     if($isUsernameValid && strlen($bio) <= 255){
         $target_dir = "uploads/";
         
+        //Get basic info from img
         $file_name = $_FILES["profileimg"]["name"];
         $tmp_name = $_FILES["profileimg"]["tmp_name"];
         $file_size = $_FILES["profileimg"]["size"];
         $file_type = $_FILES["profileimg"]["type"];
 
-        $target_file = $target_dir . basename($file_name);
-
         if($file_type == "image/png" || $file_type == "image/jpg" || $file_type == "image/jpeg"){
+            //Everything is valid, now upload and update
             
+            //Create new file name
+            $separate = explode(".", $file_name);   
+            $extension = end($separate);
+            $new_file_name = time() . "." . $extension; 
+            $target_file = $target_dir . $new_file_name;
+
+            //Resize image to 100x100
             list($width, $heigth) = getimagesize($tmp_name);
             
             $new_width = 100;
@@ -33,25 +43,48 @@ if(isset($_POST["username"]) && isset($_POST["bio"])){
             imagecopyresampled($empty_img, $this_image, 0, 0, 0, 0, $new_width, $new_heigth, $width, $heigth);
             $new_image = $empty_img;
 
-            imagepng($new_image, "../../" .$target_file);
-
-            
-            /*
-            $result = move_uploaded_file($empty_img, "../../" . $target_file);
+            //Uplaod resized image
+            if($file_type == "image/png"){
+                $result = imagepng($new_image, "../../" .$target_file);
+            }
+            else if($file_type == "image/jpg" || $file_type == "image/jpeg"){
+                $result = imagejpeg($new_image, "../../" .$target_file);
+            }
 
             if ($result) {
                 echo("img uploaded");
             }
             else{
                 echo("error while uploading");
-            }*/
-        }
-        else{
-            echo("file is not image");
-        }
+            }
 
-       
-       
+            //Insert img name to database
+            $sql = "UPDATE users SET profileimg = :pimg WHERE id = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":pimg", $new_file_name);
+            $stmt->bindParam(":id", $_SESSION["userid"]);
+            $stmt->execute();
+        
+        }
+        
+        // Inset rest of info
+        $sql = "UPDATE users 
+                SET username = :u , bio = :bio  
+                WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":u", $username);
+        $stmt->bindParam(":bio", $bio);
+        $stmt->bindParam(":id", $_SESSION["userid"]);
+
+        try{
+            $stmt->execute();
+        }
+        catch(Exception $e){
+            echo($e);
+        }
+        
+        //header("Location: http://". $_SERVER["SERVER_NAME"] . "/forumApp/index.php?page=profile&width=1");
+
     }
     else{
         echo("username or bio are not valid");    
