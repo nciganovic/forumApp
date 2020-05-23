51281f2578bec5c40d9f88a25fff7e25ec0d0cd6
nciganovic <nciganovic99@gmail.com>
2020-05-23 14:02:19 +0200

    improve redirect

--- models/user/redirect_unadmin.php
+++ models/user/redirect_unadmin.php
@@ -1,8 +1,12 @@
 <?php 
-if(!isset($_SESSION["role"])){
+if(isset($_SESSION["role"])){
     if($_SESSION["role"] == "0"){
         http_response_code(302);
         header("Location: "."http://".$_SERVER["HTTP_HOST"]."/forumApp/index.php"); #change in production
     
     }
+}
+else{
+    http_response_code(302);
+    header("Location: "."http://".$_SERVER["HTTP_HOST"]."/forumApp/index.php"); #change in production
 }
