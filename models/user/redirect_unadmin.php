f3ea92ab14d1a3df14a3faa1ba9e41f919b60e83
nciganovic <nciganovic99@gmail.com>
2020-05-23 10:27:50 +0200

    redirect no admins

---
+++ models/user/redirect_unadmin.php
@@ -0,0 +1,8 @@
+<?php 
+if(!isset($_SESSION["role"])){
+    if($_SESSION["role"] == "0"){
+        http_response_code(302);
+        header("Location: "."http://".$_SERVER["HTTP_HOST"]."/forumApp/index.php"); #change in production
+    
+    }
+}
