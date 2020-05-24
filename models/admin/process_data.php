86936df4bc97d2bc336c798ad78c76f55f516302
nciganovic <nciganovic99@gmail.com>
2020-05-24 19:11:03 +0200

    add edit option

--- models/admin/process_data.php
+++ models/admin/process_data.php
@@ -3,17 +3,15 @@ require_once "../../config/connection.php";
 require_once "get_col_names.php";
 
 $all_col_names = get_col_names($pdo, $_POST["table-name"]);
+$all_post_values = [];
+
+foreach($all_col_names as $cn){
+    if($cn[0] != "id"){
+        $all_post_values[$cn[0]] = $_POST[$cn[0]];
+    }
+}
 
 if(isset($_POST["create"])){
-    $all_post_values = [];
-
-    foreach($all_col_names as $cn){
-        if($cn[0] != "id"){
-            $all_post_values[$cn[0]] = $_POST[$cn[0]];
-            //array_push($all_post_values, $_POST[$cn[0]]);
-        }
-    }
-
     $query = "INSERT INTO " . $_POST["table-name"] . " ( ";
 
     foreach($all_post_values as $key => $value){
@@ -43,5 +41,26 @@ if(isset($_POST["create"])){
     }
 }
 else if(isset($_POST["edit"])){
-    echo("edit is set!");
+    var_dump($all_post_values);
+
+    $query = "UPDATE " . $_POST["table-name"] . " SET ";
+
+    foreach($all_post_values as $key => $value){
+        $query .= $key . " = " . "'" . $value . "'" . " , ";
+    }
+
+    $query = substr($query, 0, -3);
+
+    $query .= " WHERE  id = " .  "'" . $_POST["id"] . "'"; 
+
+    try{
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+
+        header("Location: http://localhost/forumApp/index.php?page=table&width=1&table=" . $_POST["table-name"]);
+    }
+    catch(Exception $e){
+        echo($e . "<br/>");
+        echo($query . "<br/>");
+    }
 }
