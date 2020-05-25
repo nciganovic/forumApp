496ddbe2155e7c6bf6b8f047d4418dda410183b0
nciganovic <nciganovic99@gmail.com>
2020-05-25 17:17:50 +0200

    remove echo

--- models/log/insert_log.php
+++ models/log/insert_log.php
@@ -1,6 +1,4 @@
 <?php
 $url =  "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 $current_time = time();
-echo($url);
 $myfile = file_put_contents('data/log.txt', $url . "\t" . $current_time . PHP_EOL , FILE_APPEND | LOCK_EX);
-
