8eb3b61937edf1f2f6cac4a95502291b7e23ec30
nciganovic <nciganovic99@gmail.com>
2020-05-28 16:31:28 +0200

    use new function to simplify

--- models/posts/get_more_posts.php
+++ models/posts/get_more_posts.php
@@ -2,62 +2,36 @@
 $limit = 2;
 
 require_once "../../config/connection.php";
+require_once "get_first_n_posts.php";
 
-if(isset($_GET["offset"])){
+if(isset($_GET["offset"]) && isset($_GET["order"])){
     $offset = $_GET["offset"];
 
     //getting more posts related to certain category
     if(isset($_GET["ctg"])){
         $ctg = $_GET["ctg"];
 
-        $sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
-                FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
-                INNER JOIN categories c ON p.categoryid = c.id
-                INNER JOIN users u ON p.userid = u.id
-                WHERE c.name = :ctg
-                GROUP BY p.id, p.title, p.createdat, c.name, u.username, u.id
-                ORDER BY p.createdat DESC
-                LIMIT :limit OFFSET :offset ";
-        
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
-        $stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
-        $stmt->bindValue(':ctg', $ctg);
-        $stmt->execute();
-        $all_posts = $stmt->fetchAll();
+        $posts = get_first_n_posts($ctg, $limit, $offset, $_GET["order"], $pdo);
     
         echo json_encode([
-            "posts" => $all_posts,
+            "posts" => $posts,
             "result" => "1" 
         ]); 
     }
     else{
-        $sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
-                FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
-                INNER JOIN categories c ON p.categoryid = c.id
-                INNER JOIN users u ON p.userid = u.id
-                GROUP BY p.id, p.title, p.createdat, c.name, u.username, u.id
-                ORDER BY p.createdat DESC
-                LIMIT :limit OFFSET :offset ";
 
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
-        $stmt->bindValue(':limit', (int) $limit, PDO::PARAM_INT);
-        $stmt->execute();
-        $all_posts = $stmt->fetchAll();
+        $posts = get_first_n_posts(null, $limit, $offset, $_GET["order"], $pdo);
 
         echo json_encode([
-            "posts" => $all_posts,
+            "posts" => $posts,
             "result" => "1" 
         ]); 
     }
      
-    
-
 }
 else{
     echo json_encode([
-        "msg" => "Offset parmeter does not exist.",
+        "msg" => "Offset or Order parmeter does not exist.",
         "result" => "0" 
     ]);   
 }
