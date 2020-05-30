c1ca7c762477aa682845b93e564568ce5c82d99e
nciganovic <nciganovic99@gmail.com>
2020-05-30 16:05:16 +0200

    https response code for upvote post

--- models/upvotes/upvote_post.php
+++ models/upvotes/upvote_post.php
@@ -20,18 +20,26 @@ if(isset($_POST["userID"]) && isset($_POST["postID"])){
             $stmt->bindValue(":postid", $_POST["postID"]);
             $stmt->execute();
 
+            http_response_code(201);
+
             echo json_encode(["msg" => "upvoted"]);
         }
         catch(Exception $e){
+            http_response_code(500);
+     
             echo json_encode(["msg" => $e]);
         }
     }
     else{
+        http_response_code(406);
+     
         echo json_encode(["msg" => "Already upvoted."]);    
     }
 
 }
 else{
+    http_response_code(406);
+
     echo json_encode(["msg" => "not all parameters are provided."]);
 }
 
