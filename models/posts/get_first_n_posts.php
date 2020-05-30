58a347c11310551cafc49a86a2fae7ca47b649af
nciganovic <nciganovic99@gmail.com>
2020-05-30 22:55:28 +0200

    add profileimg to query

--- models/posts/get_first_n_posts.php
+++ models/posts/get_first_n_posts.php
@@ -15,7 +15,7 @@ function get_first_n_posts($ctg, $limit, $offset, $order, $pdo){
 		$offset_query = " OFFSET :offset ";
 	}
 
-	$sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
+	$sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.profileimg, u.id as 'userid', COUNT(up.postid) AS 'likes'
 			FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
 			INNER JOIN categories c ON p.categoryid = c.id
 			INNER JOIN users u ON p.userid = u.id
