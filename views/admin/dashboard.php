5990380411a39d315f5c30839dbc4f2b8f89237f
nciganovic <nciganovic99@gmail.com>
2020-05-31 15:37:13 +0200

    add tables

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -9,32 +9,59 @@
         <div class="col-12">
             <h1 class="text-center">Admin dashboard</h1>
         </div>
+
         <div class="col-12">
-            <ul>
+            <h3 class="text-center mt-4">Tables</h3>
+        </div>
+        <div class="col-12">
+            <div class="list-group">
                 <?php foreach($all_tables as $table): ?>
-                    <li>
-                        <a href="index.php?page=table&width=1&table=<?= $table["Tables_in_forum"] ?>"> <?= $table["Tables_in_forum"] ?></a>
-                    </li>
+                    <a class="list-group-item list-group-item-action" href="index.php?page=table&width=1&table=<?= $table["Tables_in_forum"] ?>"> <?= $table["Tables_in_forum"] ?></a>
                 <?php endforeach ?>
-            </ul>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <h3 class="text-center mt-4">Page visit percent</h3>
         </div>
         <div class="col-12">
-            <h2>Website vists percent</h2>
+            <table class="table table-hover">
+                <thead>
+                    <th>Link</th>
+                    <th>Percent</th>
+                    <th>Count</th>
+                </thead>
+                <tbody>
+                    <?php foreach($urls_and_counts as $uc): ?>
+                        <tr>
+                            <td><?= $uc["url"] ?></td>
+                            <td><?= round(($uc["count"] / $total_visits) * 100 , 2) ?> %</td>  
+                            <td><?= $uc["count"] ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <div class="col-12">
+            <h3 class="text-center mt-4">Last 24h</h3>
         </div>
         <div class="col-12">
-            <ul>
-                <?php foreach($urls_and_counts as $uc): ?>
-                    <li><?= $uc["url"] ?> - <?= round(($uc["count"] / $total_visits) * 100 , 2) ?> % (<?= $uc["count"] ?>)</li>
-                <?php endforeach ?>
-            </ul>
+            <table class="table table-hover">
+                <thead>
+                    <th>Link</th>
+                    <th>Count</th>
+                </thead>
+                <tbody>
+                    <?php foreach($urls_and_counts_ld as $uc): ?>
+                        <tr>
+                            <td><?= $uc["url"] ?></td>
+                            <td><?= $uc["count"] ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
         </div>
-        <div class="col-12">
-            <h2>Last 24h</h2>
-            <ul>
-                <?php foreach($urls_and_counts_ld as $uc): ?>
-                    <li><?= $uc["url"] ?> - <?= $uc["count"] ?> </li>
-                <?php endforeach ?>
-            </ul>
-        </div>
+        
     </div>  
 </div>
