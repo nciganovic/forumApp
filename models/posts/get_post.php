4948a16eafd0d7973f7804b137507456d44977c0
nciganovic <nciganovic99@gmail.com>
2020-05-31 22:25:06 +0200

    add profileimg to post

--- models/posts/get_post.php
+++ models/posts/get_post.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_GET["id"])){
     
-    $sql = "SELECT p.id, p.title, p.description, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
+    $sql = "SELECT p.id, p.title, p.description, p.createdat, c.name, u.username, u.profileimg, u.id as 'userid', COUNT(up.postid) AS 'likes'
             FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
             INNER JOIN categories c ON p.categoryid = c.id
             INNER JOIN users u ON p.userid = u.id
