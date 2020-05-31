9ec14af984a2285f34287eb618d7599a279679fe
nciganovic <nciganovic99@gmail.com>
2020-05-31 12:59:07 +0200

    add new html

--- assets/js/search.js
+++ assets/js/search.js
@@ -83,18 +83,35 @@ $(document).ready(function(){
 
 function showPosts(data, isNew){
     var html = "";
-    
     for(d of data){
-        html += "<ul>";
-        html +=  `
-                <li>${d.id}</li>
-                <li><a href="index.php?page=post&id=${d.id}"> ${d.title} </a></li>
-                <li> ${d.name} </li>
-                <li> ${d.createdat} </li>
-                <li> ${d.username} </li>
-                <li>Likes: <span class="l-${d.id}"> ${d.likes} </span></li>
-                `
-        html += "</ul>";
+        console.log(d["profileimg"]);
+        html +=  `<a class="text-dark text-decoration-none" href="index.php?page=post&id=${d["id"]}">
+                    <div class="card mt-4 mb-4">
+                        <div class="card-header d-flex">
+                            <div class="img mr-3"> `
+
+        if(d["profileimg"]){
+            html += `<img class="rounded-circle" width="25px" src="uploads/${d["profileimg"]}" alt="profile"/>`
+        }
+        else{
+            html += `<img class="rounded-circle" width="25px" src="uploads/user.jpg" alt="profile"/>`
+        }
+                                
+        html +=`</div>
+                    <div class="username">
+                        ${d["username"]}
+                    </div>
+                    
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>${d["title"]}</p>
+                        <p> <i class="fas fa-star"></i> ${d["likes"]} </p>
+                        <footer class="blockquote-footer">${d["name"]} , ${d["createdat"]}</footer>
+                    </blockquote>
+                </div>
+            </div>
+        </a>`
     }
     
     if(isNew){
--- assets/js/sort.js
+++ assets/js/sort.js
@@ -36,16 +36,33 @@ function showFirstPosts(data){
     var html = "";
     
     for(d of data){
-        html += "<ul>";
-        html +=  `
-                <li>${d.id}</li>
-                <li><a href="index.php?page=post&id=${d.id}"> ${d.title} </a></li>
-                <li> ${d.name} </li>
-                <li> ${d.createdat} </li>
-                <li> ${d.username} </li>
-                <li>Likes: <span class="l-${d.id}"> ${d.likes} </span></li>
-                `
-        html += "</ul>";
+        html +=  `<a class="text-dark text-decoration-none" href="index.php?page=post&id=${d["id"]}">
+                    <div class="card mt-4 mb-4">
+                        <div class="card-header d-flex">
+                            <div class="img mr-3"> `
+
+        if(d["profileimg"]){
+        html += `<img class="rounded-circle" width="25px" src="uploads/${d["profileimg"]}" alt="profile"/>`
+        }
+        else{
+        html += `<img class="rounded-circle" width="25px" src="uploads/user.jpg" alt="profile"/>`
+        }
+                                
+        html +=`</div>
+                <div class="username">
+                    ${d["username"]}
+                </div>
+                
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>${d["title"]}</p>
+                    <p> <i class="fas fa-star"></i> ${d["likes"]} </p>
+                    <footer class="blockquote-footer">${d["name"]} , ${d["createdat"]}</footer>
+                </blockquote>
+            </div>
+        </div>
+        </a>`
     }
     
     $("#first-posts").html(html);
