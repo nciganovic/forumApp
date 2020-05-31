427cd71832d806fedb3072f99b172c4e57a123ff
nciganovic <nciganovic99@gmail.com>
2020-05-31 12:58:43 +0200

    improve style

--- views/posts/main.php
+++ views/posts/main.php
@@ -11,17 +11,19 @@
     }
 ?>
 
-<?php if(isset($_SESSION["userid"])): ?>    
-<div class="mt-3"><a class="p-3 btn btn-success" href="index.php?page=createpost&width=1">Create post</a></div>
-<?php endif ?>
+<div class="d-flex justify-content-between mt-4">
+    <?php if(isset($_SESSION["userid"])): ?>    
+    <div><a class="pl-3 pr-3 pt-2 pb-2 btn btn-success" href="index.php?page=createpost&width=1">Create post</a></div>
+    <?php endif ?>
 
-<div class="mt-3 mb-3">
-    <label>Order by:</label>
-    <select id="order">
-        <option  selected="selected" value="n">Newest</option>
-        <option value="o">Oldest</option>
-    </select>
+    <div>
+        <select class="form-control" id="order">
+            <option  selected="selected" value="n">Newest</option>
+            <option  value="o">Oldest</option>
+        </select>
+    </div>
 </div>
+
 <div id="first-posts">
     <?php foreach($first_n_posts as $post): ?>
         <a class="text-dark text-decoration-none" href="index.php?page=post&id=<?= $post["id"] ?>">
@@ -41,16 +43,16 @@
                 </div>
                 <div class="card-body">
                     <blockquote class="blockquote mb-0">
-                    <p><?= $post["title"] ?></p>
-                    <p> <i class="fas fa-star"></i> <?= $post["likes"] ?> </p>
-                    <footer class="blockquote-footer"><?= $post["name"] ?> , <?= $post["createdat"] ?></footer>
+                        <p><?= $post["title"] ?></p>
+                        <p> <i class="fas fa-star"></i> <?= $post["likes"] ?> </p>
+                        <footer class="blockquote-footer"><?= $post["name"] ?> , <?= $post["createdat"] ?></footer>
                     </blockquote>
                 </div>
             </div>
         </a>
     <?php endforeach ?>
 </div>
-<div id="loaded-posts" class="mt-5 mb-5"></div>
+<div id="loaded-posts"></div>
 
 <?php if(isset($_GET["ctg"])): ?>
     <p class="mt-5 mb-5"><a href="#" id="load-more" order="n" ctg="<?= $_GET["ctg"] ?>" offset="2">Load more</a></p>
