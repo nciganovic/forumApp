96a453df5241660baa965fb0d35166351f53635c
nciganovic <nciganovic99@gmail.com>
2020-05-31 12:59:24 +0200

    add profileimg to search

--- models/posts/search_post.php
+++ models/posts/search_post.php
@@ -9,7 +9,7 @@ if(isset($_GET["search"]) && isset($_GET["offset"])){
     $search = trim($_GET["search"]);
     $any = '%'.$search.'%';
 
-    $sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.id as 'userid', COUNT(up.postid) AS 'likes'
+    $sql = "SELECT p.id, p.title, p.createdat, c.name, u.username, u.profileimg, u.id as 'userid', COUNT(up.postid) AS 'likes'
             FROM posts p LEFT OUTER JOIN upvotepost up ON p.id = up.postid
             INNER JOIN categories c ON p.categoryid = c.id
             INNER JOIN users u ON p.userid = u.id
