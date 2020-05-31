bf41edd4f30170aa902e7691123a3b0fc8a6b50a
nciganovic <nciganovic99@gmail.com>
2020-05-31 14:23:53 +0200

    improve style and offset

--- views/posts/search.php
+++ views/posts/search.php
@@ -1,14 +1,14 @@
 <div class="col-12">
-    <h1 class="text-center">Search</h1>
-    <div class="form-group">
+    <h1 class="text-center m-3">Search</h1>
+    <div class="form-group d-flex mt-3">
         <input type="email" id="search-txt" class="form-control" placeholder="Search post">
-        <button class="btn btn-success mt-3 search-btn">Search</button>
+        <button class="btn btn-success search-btn ml-2">Search</button>
     </div>
     <div id="search-posts">
         
     </div>
 
-    <p class="mt-5 mb-5"><a href="#" id="loader" class="d-none load" offset="2" search="">Load more</a></p>
+    <p class="mt-5 mb-5"><a href="#" id="loader" class="d-none load" offset="10" search="">Load more</a></p>
 </div>
 
 <script src="assets/js/search.js"></script>
