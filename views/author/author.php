5fdfd1cc0bb7d3379fa7a92ca710f17bad7805e1
nciganovic <nciganovic99@gmail.com>
2020-05-31 14:50:44 +0200

    improve style and add img

--- views/author/author.php
+++ views/author/author.php
@@ -2,9 +2,17 @@
     require_once "models/author/get_bio.php";
 ?>
 
-<h1 class="text-center"> Author </h1>
-<p id="bio"> <?= $bio[0]["text"] ?> </p>
+<h1 class="text-center mt-3"> Author </h1>
 
-<a class="btn btn-info" href="models/author/export_word.php">Export</a>
+<div class="col-12 d-flex mt-5">
+    <img class="m-auto" src="assets/img/<?= $bio[0]["img"] ?>" alt="profile image" />
+</div>
+
+<p id="bio" class="text-center mt-5"> <?= $bio[0]["text"] ?> </p>
+
+<div class="col-12 d-flex justify-content-center mt-5">
+    <a class="btn btn-info" href="models/author/export_word.php">Export</a>
+</div>
+
 
 <script src="assets/js/exportWord.js"></script>
